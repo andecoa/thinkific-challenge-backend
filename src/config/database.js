09ba71a0a6600a7de8da3838c2d374653fbd69ab
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-09 20:00:07 -0700

    feat: create database initialization function

---
+++ src/config/database.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const { __MONGO_SRV__ } = require("../utils/vars");
+const logger = require("../utils/logger");
+
+const initDatabase = async () => {
+  mongoose.connection
+    .on("error", () => {
+      logger.error(`MongoDB connection error!`);
+    })
+    .once("open", () => logger.info("Connected to the database!"));
+  await mongoose.connect(__MONGO_SRV__, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  });
+};
+
+module.exports = initDatabase;
