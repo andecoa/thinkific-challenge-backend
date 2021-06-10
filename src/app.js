200497f3810d96520f2ce8daafa4f338bf76d159
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-10 01:06:15 -0700

    feat: encapsulate app by exporting it

---
+++ src/app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const routes = require("./routes");
+const handleErrors = require("./middleware/handleErrors");
+
+const app = express();
+app.use(express.json());
+
+app.use("/", routes);
+
+// all errors thrown by the routes above will be caught in this "middleware"
+app.use("/", handleErrors);
+
+// handle routes that are not found
+app.use((req, res) => {
+  res.json({
+    error: {
+      message: "route not found",
+      status: 404,
+    },
+  });
+});
+
+module.exports = app;
