bbf985b13122fd99edd188d996c8925725e70cf9
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-10 01:02:39 -0700

    chore: add variable for jwt access token secret

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -2,4 +2,6 @@ module.exports = {
   __NODE_ENV__: process.env.NODE_ENV, // dev | prod | test
   __PORT__: process.env.PORT, // e.g. 8000
   __MONGO_SRV__: process.env.MONGO_SRV, // e.g. mongodb://localhost:27017/my_db
+  // generate ATS with `require("crypto").randomBytes(64).toString("hex")` on a node shell
+  __ACCESS_TOKEN_SECRET__: process.env.ACCESS_TOKEN_SECRET,
 };
