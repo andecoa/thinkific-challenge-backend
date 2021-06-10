0948ecd2434daf290e94bd4ce625ac3fea984113
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-10 01:56:34 -0700

    bug: disable pino-pretty on non-dev environments

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,10 @@
 const pino = require("pino");
+const { __NODE_ENV__ } = require("./vars");
 
-const logger = pino({
-  prettyPrint: { colorize: true, ignore: "pid,hostname" },
-});
+const logger = pino(
+  __NODE_ENV__ === "dev" && {
+    prettyPrint: { colorize: true, ignore: "pid,hostname" },
+  }
+);
 
 module.exports = logger;
