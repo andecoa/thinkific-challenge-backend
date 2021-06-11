f2fdec86a9ec4ee2dc00d930eda2d20a309fd309
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-11 03:17:36 -0700

    feat: add scopes for userInteger

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
+const jwtAuthz = require("express-jwt-authz");
 const apiV1 = require("./v1");
 const checkJwt = require("../middleware/checkJwt");
 
+const checkScopes = jwtAuthz(["read:userInteger", "write:userInteger"]);
+
 // all the routes are protected
-router.use("/v1", [checkJwt, apiV1]);
+router.use("/v1", [checkJwt, checkScopes, apiV1]);
 
 module.exports = router;
