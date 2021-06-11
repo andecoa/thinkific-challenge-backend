c795c17919ba2485c26f4acf9396b24e800a82fb
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-11 03:27:17 -0700

    chore: add cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const routes = require("./routes");
 const handleErrors = require("./middleware/handleErrors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use("/", routes);
 
