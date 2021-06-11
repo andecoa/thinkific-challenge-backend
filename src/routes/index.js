87690de52a4df05c71be8195adf276ad7b158c1a
Angelo Aldecoa <angelokn14@gmail.com>
2021-06-10 23:41:53 -0700

    feat: protect resource routes with auth0

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
 const apiV1 = require("./v1");
+const checkJwt = require("../middleware/checkJwt");
 
-router.use("/v1", apiV1);
+// all the routes are protected
+router.use("/v1", [checkJwt, apiV1]);
 
 module.exports = router;
